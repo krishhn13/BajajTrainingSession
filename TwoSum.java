1cda6623609901b81db2f1d17611b3afabef98c5
Krishan <chiranjiv.shah783@gmail.com>
2026-05-08 21:09:44 +0530

    Added New Set Of Questions

--- TwoSum.java
+++ TwoSum.java
@@ -1,15 +1,18 @@
 import java.util.*;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0 ; i<nums.length;i++) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
             int diff = target - nums[i];
-            if(map.containsKey(diff)) {
-                return new int[]{map.get(diff),i};
+            if (map.containsKey(diff)) {
+                return new int[] { map.get(diff), i };
             }
-            map.put(nums[i],i);
+            map.put(nums[i], i);
         }
-        return new int[]{-1,-1};
+        return new int[] { -1, -1 };
     }
 }
-public class TwoSum{}
+
+public class TwoSum {
+}
