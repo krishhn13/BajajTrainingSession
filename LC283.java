90514d5983541e28e163e2a0c4dd3d71a5bd8437
Krishan <chiranjiv.shah783@gmail.com>
2026-05-08 21:23:21 +0530

    Added New Set Of Questions

---
+++ LC283.java
@@ -0,0 +1,17 @@
+class Solution {
+        public void moveZeroes(int[] nums) {
+                int i = 0;
+                for (int j = 0; j < nums.length; j++) {
+                        if (nums[j] != 0) {
+                                int temp = nums[j];
+                                nums[j] = nums[i];
+                                nums[i] = temp;
+                                i++;
+                        }
+                }
+        }
+}
+
+public class LC283 {
+
+}
---
+++ LC455.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g); Arrays.sort(s);
+        int i = 0  ,j = 0, count = 0;
+        while(i<g.length && j<s.length) {
+            if(s[j] >= g[i]) {
+                count++; j++; i++;
+            }
+            else {
+                j++;
+            }
+        }
+        return count;
+    }
+}
+public class LC455 {
+        
+}
