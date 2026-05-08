0007c40ea22e8db6a8cf6f2670ef3caceaf4f401
Krishan <chiranjiv.shah783@gmail.com>
2026-05-08 21:38:38 +0530

    Added New Solutions

---
+++ LC142.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {
+                slow = head;
+                while(slow!=fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
